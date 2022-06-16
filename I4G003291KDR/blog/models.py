90bf0fb60cd0f13e7688d194cf7516c972ac4615
CodeMarshal007 <102981590+CodeMarshal007@users.noreply.github.com>
2022-06-16 15:16:26 -0700

    my blog

---
+++ I4G003291KDR/blog/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+User = get_user_model()
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
